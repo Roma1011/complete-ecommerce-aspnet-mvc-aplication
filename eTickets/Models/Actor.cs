70308fe06c976a054d549bb51040bf7daf2e3307
Roma <rkekua9@gmail.com>
2023-04-11 12:41:01 +0400

    Cretae Actor Model

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+	public class Actor
+	{
+		[Key]
+		public int Id { get; set; }
+		public string ProfilePictureURL { get; set; }
+		public string Fulkname { get; set; }
+		public string Bio { get; set; }
+
+	}
+}
